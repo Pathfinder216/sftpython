1f806cacb2c60052911621ad7870e6062bb348f3
Ben Green <benno.green@gmail.com>
2023-04-08 16:31:23 -0400

    Add pylint

--- sftpython/__init__.py
+++ sftpython/__init__.py
@@ -1,2 +1,5 @@
-class Connection:
-    pass
+"""SFTP functionality"""
+
+
+class Connection:  # pylint: disable=too-few-public-methods
+    """Connection"""
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,5 +1,7 @@
+"""Tests"""
 from sftpython import Connection
 
 
 def test_initialize() -> None:
+    """Test creating a Connection object"""
     Connection()

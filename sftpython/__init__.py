a7ca13ccd6e2fa2a1326a10c22b6fd48681c700a
Ben Green <benno.green@gmail.com>
2023-04-08 22:19:50 -0400

    Add __version__ to __init__

--- sftpython/__init__.py
+++ sftpython/__init__.py
@@ -1,4 +1,7 @@
 """SFTP functionality"""
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__package__ or __name__)
 
 
 class Connection:  # pylint: disable=too-few-public-methods

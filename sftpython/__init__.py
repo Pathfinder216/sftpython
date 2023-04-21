58ef3046b1c5d50d6880631f50d029d2799f9a08
Ben Green <benno.green@gmail.com>
2023-04-20 23:40:25 -0400

    Fix init.py

--- sftpython/__init__.py
+++ sftpython/__init__.py
@@ -1,7 +1,12 @@
 """SFTP functionality"""
-import importlib.metadata
+try:
+    import importlib.metadata
 
-__version__ = importlib.metadata.version(__package__ or __name__)
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except ImportError:
+    from pkg_resources import get_distribution
+
+    __version__ = get_distribution(__package__ or __name__).version
 
 
 class Connection:  # pylint: disable=too-few-public-methods

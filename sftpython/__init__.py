7adfd4cc31c0ba11f62ba855e605f8eb7b469294
Ben Green <benno.green@gmail.com>
2023-04-20 23:47:23 -0400

    Proper python version check

--- sftpython/__init__.py
+++ sftpython/__init__.py
@@ -1,9 +1,11 @@
 """SFTP functionality"""
-try:
+import sys
+
+if sys.version_info >= (3, 8, 0):
     import importlib.metadata
 
     __version__ = importlib.metadata.version(__package__ or __name__)
-except ImportError:
+else:
     from pkg_resources import get_distribution
 
     __version__ = get_distribution(__package__ or __name__).version
